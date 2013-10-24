c4c6a52184741584080532486045111404b60fe6
werdyl <eddywerdy@gmail.com>
2013-10-23 20:09:10 -0400

    added favicon and removed extra images

---
+++ db/migrate/20131023234550_add_description_column_to_blog_post.rb
@@ -0,0 +1,5 @@
+class AddDescriptionColumnToBlogPost < ActiveRecord::Migration
+	def change
+		add_column :posts, :description, :string
+	end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131008213547) do
+ActiveRecord::Schema.define(:version => 20131023234550) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.string   "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.string   "description"
   end
 
 end
